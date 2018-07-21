af2cf40a9cb362a56dc8d73151690f33b8fee51a
Raju Athokpam <raju.athokpam@gmail.com>
2018-07-21 12:22:16 +0530

    aa

---
+++ src/medium/com/SortBinaryDataTypeArray.java
@@ -0,0 +1,20 @@
+package medium.com;
+
+/**
+ * Created by Raju on 5/26/2018.
+ */
+public class SortBinaryDataTypeArray {
+
+    private void sort(){
+        boolean[] arr = {false, true, false, true, false, true, false, true};
+
+        int start = 0;
+        int end = arr.length;
+
+        while(start < end){
+            if(arr[start]){
+
+            }
+        }
+    }
+}
