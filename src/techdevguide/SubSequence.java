1a195f4feec96ea62eaf260f29a1ddf9d79d6b96
raju.athokpam <raju.athokpam@gmail.com>
2018-07-25 16:45:35 +0530

    a

---
+++ src/techdevguide/SubSequence.java
@@ -0,0 +1,32 @@
+package techdevguide;
+
+import java.util.HashMap;
+
+public class SubSequence {
+
+    String S = "abppplee";
+    String[] D = {"able", "ale", "apple", "bale", "kangaroo"};
+
+    void find() {
+        HashMap<Integer, Integer> indexCount = new HashMap<>();
+        for (int i = 0; i < D.length; i++) {
+            String word = D[i];
+            int iS = 0;
+            boolean isSubsequnce = true;
+            for (int j = 0; j < word.length(); j++) {
+                for (int k = iS; k < S.length(); k++) {
+                    if (word.charAt(j) == S.charAt(k)) {
+                        iS = k;
+                        break;
+                    }
+                }
+                if (iS == S.length() - 1) {
+                    isSubsequnce = false;
+                }
+            }
+            if (isSubsequnce) {
+                indexCount.put(i, word.length());
+            }
+        }
+    }
+}
